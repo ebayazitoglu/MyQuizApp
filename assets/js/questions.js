acad573aeb3fcc525e17af7b8db41158711dabf9
Ebubekir Bayazitoglu <ebayazitoglu@gmail.com>
2023-01-20 17:46:01 +0000

    Developed the code and updated github

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+        question: "Which one is NOT a data type?",
+        answers: ["var", "string","number"],
+        correctAnswer: "var"
+    },
+    {
+        question: "How do you enable a click action with event listeners?",
+        answers: ["click","keydown","mouseover"],
+        correctAnswer: "click"
+    },
+    {
+        question: "Which are not loop types?",
+        answers: ["for","forEach","loop"],
+        correctAnswer: "loop"
+    },
+    {
+        question: "which method add an item to an array?",
+        answers: ["contain","push","add"],
+        correctAnswer: "push"
+    }
+]
